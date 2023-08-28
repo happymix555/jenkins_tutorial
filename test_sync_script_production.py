ee111c2644f69a35b3149caef76439a9050fa28d
happymix <happymix2555@gmail.com>
2023-08-28 11:45:58 +0700

    initial commit

---
+++ test_sync_script_production.py
@@ -0,0 +1,23 @@
+import argparse
+
+# program description
+ProgramDescriptionStr = 'This script is used in production'
+
+# parse argument
+parser = argparse.ArgumentParser( description=ProgramDescriptionStr )
+parser.add_argument( '--testArgumentStr', type=str,
+					help='input any word you want...',
+					default='test argument of production script' )
+
+# get argument
+args = parser.parse_args()
+testArgumentStr = args.testArgumentStr
+
+# open config file 
+with open( 'test_config_production.txt' ) as productionConfigFile:
+
+	# store every line of config file as list
+	allConfigLineList = productionConfigFile.readlines()
+
+# inform user
+print( '[production script] argument: {}, config: {}'.format( testArgumentStr, allConfigLineList[ 0 ] ) )
