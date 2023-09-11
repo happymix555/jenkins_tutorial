b86030ee9da2c0f91c53e582b17684370db0f1d5
mixpak <mixpak@yannix.com>
2023-09-11 18:38:19 +0700

    delete and rename file

--- command_to_run_sync_script.sh
+++ command_to_run_sync_script.sh
@@ -3,23 +3,23 @@
 # This is a Bash script to run a Python script.
 
 # path to python scritp
-testSyncScriptProductionPath="test_sync_script_production.py"
+SyncScriptFilePath="sync_script.py"
 
-# Check if test_sync_script_production.py exists
-if [ -f "$testSyncScriptProductionPath" ]; then
+# Check if sync script exists
+if [ -f "$SyncScriptFilePath" ]; then
 
 	# inform user
-	echo "Running the Python script: $testSyncScriptProductionPath"
+	echo "Running the Python script: $SyncScriptFilePath"
 
 	# run python script
-	python3 "$testSyncScriptProductionPath" --testArgumentStr="1"
-	python3 "$testSyncScriptProductionPath" --testArgumentStr="2"
-	python3 "$testSyncScriptProductionPath" --testArgumentStr="3"
-	python3 "$testSyncScriptProductionPath" --testArgumentStr="4"
+	python3 "$SyncScriptFilePath" --testArgumentStr="1"
+	python3 "$SyncScriptFilePath" --testArgumentStr="2"
+	python3 "$SyncScriptFilePath" --testArgumentStr="3"
+	python3 "$SyncScriptFilePath" --testArgumentStr="4"
 
 # script was not found
 else
 
 	# inform user
-	echo "Python script not found: $testSyncScriptProductionPath"
+	echo "Python script not found: $SyncScriptFilePath"
 fi
